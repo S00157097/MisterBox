638b7ccf153d3e88e016553230c0d5d952e89828
monstrasitix <dmitrijssavostijanovs@yahoo.ie>
2015-12-21 16:31:31 +0000

    Products Finished

--- products.php
+++ products.php
@@ -1,29 +1,64 @@
 <?PHP
     require_once('db.php');
 
-    $list = array(
-        array(
-            '<div class="item" style="background-image: url(media/boxes/F/F1.jpg);">',
-            '<span class="type">F1</span>',
-            '<span class="price">11.11</span>',
-            '<span class="width">99 x 99 x 99</span></div>'
-             ),
-        array(
-            '<div class="item" style="background-image: url(media/ribbons/3/30110C.jpg);">',
-            '<span class="price">11.11</span>',
-            '<span class="width">Width: 00cm</span></div>'
-        ),
-        array(
-            '<div class="item" style="background-image: url(media/bows/0240.jpg);">',
-            '<span class="price">11.11</span>',
-            '<span class="width">000mm</span></div>'
-        ),
-        array(
-            '<div class="item" style="background-image: url(media/butterflies/5/388.jpg);">',
-            '<span class="price">99.99</span>',
-            '<span class="width">Width: 10cm</span></div>'
-        )
-    );
+    $boxType = array();
+    $boxSize = array();
+    $boxWidth = array();
+    $boxPrice = array();
+
+    $sql = "SELECT BoxType, BoxSize, BoxLength, BoxWidth, BoxHeight, Price FROM BoxTbl;";
+    $result = mysqli_query($dbc, $sql);
+    $i = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $boxType[$i] = $row['BoxType'];
+        $boxSize[$i] = $row['BoxSize'];
+        $boxWidth[$i] = $row['BoxLength'] .' x '. $row['BoxWidth'] .' x '. $row['BoxHeight'];
+        $boxPrice[$i] = $row['Price'];
+        $i++;
+    }
+          
+            
+    $ribbonId = array();
+    $ribbonWidth = array();
+    $ribbonPrice = array();
+
+    $sql = "SELECT RibbonID, Width, Price FROM RibbonTbl;";
+    $result = mysqli_query($dbc, $sql);
+    $i = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $ribbonId[$i] = $row['RibbonID'];
+        $ribbonWidth[$i] = $row['Width'];
+        $ribbonPrice[$i] = $row['Price'];
+        $i++;
+    }
+
+    $bowId = array();
+    $bowWidth = array();
+    $bowPrice = array();
+    
+    $sql = "SELECT BowID, Width, Price FROM BowTbl;";
+    $result = mysqli_query($dbc, $sql);   
+    $i = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $bowId[$i] = $row['BowID'];
+        $bowWidth[$i] = $row['Width'];
+        $bowPrice[$i] = $row['Price'];
+        $i++;
+    }
+
+    $butterId = array();
+    $butterWidth = array();
+    $butterPrice = array();
+    
+    $sql = "SELECT ButterflyID, Width, Price FROM ButterflyTbl;";
+    $result = mysqli_query($dbc, $sql);    
+    $i = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $butterId[$i] = $row['ButterflyID'];
+        $butterWidth[$i] = $row['Width'];
+        $butterPrice[$i] = $row['Price'];
+        $i++;
+    }
 
 ?>
 
@@ -52,12 +87,42 @@
                 <div id="items">
                     <?PHP               
                         for($i = 0; $i < 15; $i++) {
-                            $rNum = rand(0,3);
+                            $rand = rand(0, 3);
                             
-                            echo $list[$rNum][0];
-                            echo $list[$rNum][1];
-                            echo $list[$rNum][2];
-                            if($rNum == 0) echo $list[$rNum][3];
+                            switch($rand) {
+                                case 0:
+                                    $x = rand(0, sizeof($boxSize) - 1);
+                                    echo '<div class="item" style="background-image: url(media/boxes/'. $boxType[$x] .'/'. $boxType[$x] .'1.jpg);">';
+                                    echo '<span class="type">'. $boxSize[$x] .'</span>';
+                                    echo '<span class="price">'. $boxPrice[$x] .'</span>';
+                                    echo '<span class="width">'.  $boxWidth[$x] .'</span>';
+                                    echo '</div>';
+                                    break;
+                                    
+                                case 1:
+                                    $x = rand(0, sizeof($ribbonId) - 1);
+                                    echo '<div class="item" style="background-image: url(media/ribbons/'. $ribbonWidth[$x] .'/'. $ribbonId[$x] .'.jpg);">';
+                                    echo '<span class="price">'. $ribbonPrice[$x] .'</span>';
+                                    echo '<span class="width">Width: '.  $ribbonWidth[$x] .'cm</span>';
+                                    echo '</div>';
+                                    break;
+                                    
+                                case 2:
+                                    $x = rand(0, sizeof($bowId) - 1);
+                                    echo '<div class="item" style="background-image: url(media/bows/'. $bowId[$x] .'.jpg);">';
+                                    echo '<span class="price">'. $bowPrice[$x] .'</span>';
+                                    echo '<span class="width">'. $bowWidth[$x] .'mm</span>';
+                                    echo '</div>';
+                                    break;
+                                    
+                                case 3:
+                                    $x = rand(0, sizeof($butterId) - 1);
+                                    echo '<div class="item" style="background-image: url(media/butterflies/'. $butterWidth[$x] .'/'. $butterId[$x] .'.jpg);">';
+                                    echo '<span class="price">'. $butterPrice[$x] .'</span>';
+                                    echo '<span class="width">Width: '. $butterWidth[$x] .'cm</span>';
+                                    echo '</div>';
+                                    break;
+                            }
                         }
                     ?>
                 </div>
